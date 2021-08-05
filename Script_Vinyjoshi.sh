fdafec9b49c74055370aada46eaa8cd9f74c75f8
ivyihsoj <88343534+ivyihsoj@users.noreply.github.com>
2021-08-05 13:23:33 +0530

    Update Script_Vinyjoshi.sh

--- Script_Vinyjoshi.sh
+++ Script_Vinyjoshi.sh
@@ -14,4 +14,4 @@ for (( i=0; i<${#Slackusername}; i++ )); do
   fi
 done
 
-echo -e "$Name,$Email,$Slackusername,$Biostack,$Twitter,$distance "
+echo "$Name,$Email,$Slackusername,$Biostack,$Twitter,$distance "
