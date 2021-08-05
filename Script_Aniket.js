562fc4b893990f3f725d63291854178a20b7c62c
ivyihsoj <88343534+ivyihsoj@users.noreply.github.com>
2021-08-05 12:41:26 +0530

    Add files via upload

---
+++ Script_Aniket.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+<script>
+
+function hammingDist(str1, str2)
+{
+    let i = 0, count = 0;
+    while (i < str1.length)
+    {
+        if (str1[i] != str2[i])
+            count++;
+        i++;
+    }
+    return count;
+}
+    var Name = "Aniket";
+    var Email = "aniket@something.com";
+    var Slackusername = "@Viny_joshi";
+    var Biostack = "Something";
+    var Twitter = "@viny-joshi"
+
+    document.write(Name +',' + Email +','+ Slackusername +','+ Biostack + ","+ Twitter+ ","+ hammingDist (Slackusername, Twitter));
+
+</script>
---
+++ Script_muskan.R
@@ -0,0 +1,17 @@
+#1/usr/bin/Rscript
+
+name <- "Vinay Joshi"
+email <- "viyj2000@gmail.com"
+slackUsername <- "@Vinyjoshi"
+biostack <- "Fill Here"
+twitter <- "@vinyjoshi"
+
+a = unlist(strsplit(slackUsername, ""))
+b = unlist(strsplit(twitter, ""))
+
+distance <- sum(a != b)
+
+
+my_details <- c(name, email, slackUsername, biostack,twitter,distance)
+  
+cat(paste(my_details, collapse = ','))
