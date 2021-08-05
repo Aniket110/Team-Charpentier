baa35088592f26f50ce3b3b996875fabd241c5d6
ivyihsoj <88343534+ivyihsoj@users.noreply.github.com>
2021-08-05 13:16:51 +0530

    Update Script_jeel.cpp

--- Script_jeel.cpp
+++ Script_jeel.cpp
@@ -1,4 +1,4 @@
-#!/usr/bin/gcc
+///bin/true;COMPILER_OPTIONS="-g -Wall -Wextra --std=c++14 -O1 -fsanitize=address,undefined";THIS_FILE="$(cd "$(dirname "$0")"; pwd -P)/$(basename "$0")";OUT_FILE="/tmp/build-cache/$THIS_FILE";mkdir -p "$(dirname "$OUT_FILE")";test "$THIS_FILE" -ot "$OUT_FILE" || $(which clang++ || which g++) -xc++ $COMPILER_OPTIONS "$THIS_FILE" -o "$OUT_FILE" || exit;exec "$OUT_FILE" "$@"
 
 #include <iostream>
 #include <cstring>
